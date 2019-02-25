588992a9346a29c28fd79d58b60b7002f0e9140d
GitHubAlexKir <alexvanfontys@gmail.com>
2019-02-25 11:01:20 +0100

    Added testing

---
+++ src/test/java/AccountControllerTest.java
@@ -0,0 +1,25 @@
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.get;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class AccountControllerTest {
+
+    @BeforeClass
+    public static void init() {
+        RestAssured.baseURI = "http://localhost/1";
+        RestAssured.port = 8080;
+    }
+
+    @Test
+    public void testUserFetchesSuccess() {
+        get("/api/account/get/1")
+                .then()
+                .body("id", equalTo(1))
+                .body("email", equalTo("a@h.nl"))
+                .body("name", equalTo("Alex"))
+                .body("password", equalTo("123456789011"));
+    }
+}
