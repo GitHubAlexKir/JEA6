4aa12b2657b9eb65d4fcbb6dfb422e240aba4999
GitHubAlexKir <alexvoorfontys@gmail.com>
2019-02-28 13:48:22 +0100

    TDD: Added Item Repository

---
+++ src/main/java/Repository/ItemRepository.java
@@ -0,0 +1,39 @@
+package Repository;
+
+import Interceptor.SimpleInterceptor;
+import domain.Item;
+import domain.Person;
+
+import javax.ejb.Stateless;
+import javax.interceptor.Interceptors;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+import static javax.transaction.Transactional.TxType.REQUIRED;
+
+@Stateless
+
+@Interceptors(SimpleInterceptor.class)
+public class ItemRepository {
+
+   @PersistenceContext(unitName = "myPU")
+   private EntityManager em;
+
+   @Transactional(REQUIRED)
+   public void create(Item item) {
+       System.out.println(item.toString());
+       em.persist(item);
+   }
+
+   public List<Item> findAll() {
+       return em.createQuery("SELECT i FROM Item i", Item.class)
+                      .getResultList();
+   }
+
+   public Item find(Long id) {
+       return em.find(Item.class, id);
+   }
+
+}
