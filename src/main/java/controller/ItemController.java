14cc7d331b34aa41777592c5bb1ced664eba701f
GitHubAlexKir <alexvoorfontys@gmail.com>
2019-02-28 13:46:16 +0100

    TDD: Added Item Controller

---
+++ src/main/java/controller/ItemController.java
@@ -0,0 +1,47 @@
+package controller;
+
+import domain.Item;
+
+import javax.ejb.EJB;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/item")
+public class ItemController {
+    @EJB
+    ItemRepository repo;
+
+    public ItemController()
+    {
+
+    }
+    @GET
+    public String item()
+    {
+        return "welkom bij items";
+    }
+    @GET
+    @Path("/get/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Item getItem(@PathParam("id") long id)
+    {
+        return repo.find(id);
+    }
+
+    @GET
+    @Path("/get/all")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Item> getItems()
+    {
+        return repo.findAll();
+    }
+
+    @POST
+    @Path("/save")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String save(Item item) {
+        return repo.create(item);
+    }
+}
